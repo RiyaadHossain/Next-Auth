3949bd11f66b06e486983dc615d489f703de7db6
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-16 09:59:54 +0600

    make home page restricted free

--- pages/index.js
+++ pages/index.js
@@ -57,27 +57,3 @@ function User({ session }) {
     </main>
   )
 }
-
-export async function getServerSideProps({ req }) {
-  const session = await getSession({ req })
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false
-      }
-    }
-  }
-
-  return { props: session }
-}
-
-/*   if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false
-      }
-    }
-  } */
