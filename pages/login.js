7018c7cffdb1eb0d9d5d8cde7251a960a5003cb5
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-16 08:46:45 +0600

    google and github login added

--- pages/login.js
+++ pages/login.js
@@ -5,72 +5,82 @@ import styles from '../styles/Form.module.css';
 import Image from 'next/image'
 import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
 import { useState } from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Login(){
+export default function Login() {
+    console.log(process.env.GITHUB_SECRET)
+    const { data: session } = useSession()
+    const [show, setShow] = useState()
 
-    const [show, setShow] = useState(false)
+    const signInWithGoogle = () => {
+        signIn('google', { callbackUrl: 'http://localhost:3000' })
+    }
+
+    const signInWithGitHub = () => {
+        signIn('github', { callbackUrl: 'http://localhost:3000' })
+    }
 
     return (
         <Layout>
 
-        <Head>
-            <title>Login</title>
-        </Head>
-        
-        <section className='w-3/4 mx-auto flex flex-col gap-10'>
-            <div className="title">
-                <h1 className='text-gray-800 text-4xl font-bold py-4'>Explore</h1>
-                <p className='w-3/4 mx-auto text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia?</p>
-            </div>
+            <Head>
+                <title>Login</title>
+            </Head>
 
-            {/* form */}
-            <form className='flex flex-col gap-5'>
-                <div className={styles.input_group}>
-                    <input 
-                    type="email"
-                    name='email'
-                    placeholder='Email'
-                    className={styles.input_text}
-                    />
-                    <span className='icon flex items-center px-4'>
-                        <HiAtSymbol size={25} />
-                    </span>
-                </div>
-                <div className={styles.input_group}>
-                    <input 
-                    type={`${show ? "text" : "password"}`}
-                    name='password'
-                    placeholder='password'
-                    className={styles.input_text}
-                    />
-                     <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
-                        <HiFingerPrint size={25} />
-                    </span>
+            <section className='w-3/4 mx-auto flex flex-col gap-10'>
+                <div className="title">
+                    <h1 className='text-gray-800 text-4xl font-bold py-4'>Explore</h1>
+                    <p className='w-3/4 mx-auto text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia?</p>
                 </div>
 
-                {/* login buttons */}
-                <div className="input-button">
-                    <button type='submit' className={styles.button}>
-                        Login
-                    </button>
-                </div>
-                <div className="input-button">
-                    <button type='button' className={styles.button_custom}>
-                        Sign In with Google <Image src={'/assets/google.svg'} width="20" height={20} ></Image>
-                    </button>
-                </div>
-                <div className="input-button">
-                    <button type='button' className={styles.button_custom}>
-                        Sign In with Github <Image src={'/assets/github.svg'} width={25} height={25}></Image>
-                    </button>
-                </div>
-            </form>
+                {/* form */}
+                <form className='flex flex-col gap-5'>
+                    <div className={styles.input_group}>
+                        <input
+                            type="email"
+                            name='email'
+                            placeholder='Email'
+                            className={styles.input_text}
+                        />
+                        <span className='icon flex items-center px-4'>
+                            <HiAtSymbol size={25} />
+                        </span>
+                    </div>
+                    <div className={styles.input_group}>
+                        <input
+                            type={`${show ? "text" : "password"}`}
+                            name='password'
+                            placeholder='password'
+                            className={styles.input_text}
+                        />
+                        <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
+                            <HiFingerPrint size={25} />
+                        </span>
+                    </div>
 
-            {/* bottom */}
-            <p className='text-center text-gray-400 '>
-                don't have an account yet? <Link href={'/register'}><a className='text-blue-700'>Sign Up</a></Link>
-            </p>
-        </section>
+                    {/* login buttons */}
+                    <div className="input-button">
+                        <button type='submit' className={styles.button}>
+                            Login
+                        </button>
+                    </div>
+                    <div className="input-button">
+                        <button onClick={signInWithGoogle} type='button' className={styles.button_custom}>
+                            Sign In with Google <Image src={'/assets/google.svg'} width="20" height={20} alt=''></Image>
+                        </button>
+                    </div>
+                    <div className="input-button">
+                        <button onClick={signInWithGitHub} type='button' className={styles.button_custom}>
+                            Sign In with Github <Image src={'/assets/github.svg'} width={25} height={25} alt=''></Image>
+                        </button>
+                    </div>
+                </form>
+
+                {/* bottom */}
+                <p className='text-center text-gray-400 '>
+                    dont have an account yet? <Link href={'/register'}><a className='text-blue-700'>Sign Up</a></Link>
+                </p>
+            </section>
 
         </Layout>
     )
